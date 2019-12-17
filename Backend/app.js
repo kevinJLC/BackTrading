42755ed1def194f4acd199e62be18e198dae80d7
Inti <intimartinezfloresicube@gmail.com>
2019-12-17 10:10:12 -0600

    Solucion a problema CORS
    Se añadio el codigo para controlar los errores CORS en app.js

--- Backend/app.js
+++ Backend/app.js
@@ -14,6 +14,18 @@ then(()=>{
 });
 //Middlewares
 
+  app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-type, Accept"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST,PATCH, PUT, DELETE, OPTIONS"
+    );
+    next();
+  });
 
 //Rutas
 app.use(routes);
