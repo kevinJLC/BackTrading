4c52f0f5b6473caaadd391eb9fc2ff8a3b79745c
{{kevin.name}} <lopezcarrillokevin36@gmail.com>
2020-01-06 20:05:21 -0600

    Envio de correo para más de 4 intentos de inicio de sesion
    Enviamos un correo cada vez que se detecten 4 intento de sesión fallidos

--- Backend/controllers/login.controller.js
+++ Backend/controllers/login.controller.js
@@ -1,6 +1,9 @@
 const Usuario=require('../models/Usuarios');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
+var sgTransport = require('nodemailer-sendgrid-transport');
+const eventemither = require ('events').EventEmitter
 const controller = {};
 var intentos = 0;
 
@@ -28,7 +31,45 @@ controller.postUser=(req,res) => {
         if(intentos>4){
           intentos=0;
           // Envio de correo
-          
+
+          var options = {
+
+        auth: {
+
+          api_user: 'kevin36joel',
+
+          api_key: '333ingeniero777'
+
+        }
+
+          }
+          var client = nodemailer.createTransport(sgTransport(options));
+
+          var email = {
+
+            from: 'Baktesting',
+
+            to: 'lopezcarrillokevin36@gmail.com',
+
+            subject: ' Alerta de seguridad [Intentos de inicio de sesion]',
+
+            html: '<b> Se ha intentado iniciar sesión en tu cuenta más de 4 veces seguidas sin éxito</b> <br> <p> <strong> Si fuiste tú, ignora este mensaje  </strong> </p>' };
+          client.sendMail(email, function(err, info){
+
+            if (err ){
+
+            console.log(err);
+
+            } else {
+
+            console.log('Message sent: ' + info.response);
+
+        }
+
+          });
+
+          console.log('Ve a tu correo y verifica tu cuenta');
+
         }
 
         Usuario.findByIdAndUpdate(fetchuser._id,{intentos: intentos}).then(res => {console.log(res)}).catch(err => {console.log(err)});
@@ -46,7 +87,5 @@ controller.postUser=(req,res) => {
       return res.status(403).json({ message: 'Autentificación inválida'});
     });
 }
-
-
 
 module.exports=controller;
