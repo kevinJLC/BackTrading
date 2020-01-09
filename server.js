c52aebb41e942d1e4b4332e1f0c3485dd738f80c
{{kevin.name}} <lopezcarrillokevin36@gmail.com>
2020-01-09 13:09:43 -0600

    Punto de restauración antes de unir servers
    Puedes estar tranquilo, si restauras a este punto todo funcionará en 2 servidores distintos hasta el día 09/01/2019, Bienvenidos sean si todo se fue a la mierda

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+/*
+09/01/2019 12:31am GMT: este es el último commit antes de usar el localhost:3000 como único server,
+abandonaremos el localhost 4200, para ello cuando se haga una petición get al localhost:3000 se lanzará
+el index.html (archivos estáticos) que se generaron con el comando ng-build.
+Hasta este punto actualizamos el ng-update y todo funciona bien, cualquier error fatal
+que ocurra en el futuro y nos obligue abortar mision, volveremos aquí con nueva información.
+que dios nos bendiga y que chingue a su madre el América.
+*/
 const app = require("./backend/app");
 const debug = require("debug")("node-angular");
 const http = require("http");
