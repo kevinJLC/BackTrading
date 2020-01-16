bea68679347c383cc616854c5be0a453476e491e
{{kevin.name}} <lopezcarrillokevin36@gmail.com>
2020-01-16 14:42:57 -0600

    as

--- src/app/servicios/login.service.ts
+++ src/app/servicios/login.service.ts
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
   providedIn: 'root'
 })
 export class LoginService {
-  readonly URL_API = 'http://localhost:3000/api/login';
+  readonly URL_API = '/api/login';
 
   private token: string;
   private tokenTimer: any;    // NodeJS.timer
