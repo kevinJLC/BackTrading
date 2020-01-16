c0f554437a4c84b8aac504a1f8c8c509cbee5e6e
{{kevin.name}} <lopezcarrillokevin36@gmail.com>
2020-01-16 01:20:48 -0600

    a

--- Backend/server.js
+++ Backend/server.js
@@ -57,4 +57,6 @@ app.set("port", port);
 const server = http.createServer(app);
 server.on("error", onError);
 server.on("listening", onListening);
-server.listen(port);
+server.listen(port,() => {
+  console.log('listening on port ' + port);
+});
